65700489f0f75a87b7e629c5ffcce016276bb12b
JPGodinho <jpgpassiani@gmail.com>
2026-03-11 22:30:07 -0300

    Aula inicial

---
+++ src/Produto.java
@@ -0,0 +1,12 @@
+public class Produto {
+
+    double preco;
+    String nome;
+    int estoque;
+
+    void exibir(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Preco: " + preco);
+        System.out.println("Estoque: " + estoque);
+    }
+}
---
+++ src/TesteProduto.java
@@ -0,0 +1,11 @@
+public class TesteProduto {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto();
+
+        produto1.nome = "pizza calabresa";
+        produto1.preco = 12.5;
+        produto1.estoque = 1;
+
+        produto1.exibir();
+    }
+}
