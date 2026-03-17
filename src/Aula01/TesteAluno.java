0c54ed1770956286a5d24b3387cad7806c359240
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 22:29:22 -0300

    feat: adicionando objeto

--- src/Aula01/TesteAluno.java
+++ src/Aula01/TesteAluno.java
@@ -3,11 +3,18 @@ package Aula01;
 public class TesteAluno {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
 
         aluno1.nome = "Godinho";
         aluno1.curso = "Engenharia de Software";
         aluno1.idade = 20;
 
+        aluno2.nome = "Vitor";
+        aluno2.curso = "Engenharia de Software";
+        aluno2.idade = 19;
+
         aluno1.apresentar();
+        System.out.println();
+        aluno2.apresentar();
     }
 }
