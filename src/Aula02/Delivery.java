01fe9c29ecdfad67a8237ab2d57f7de15b40bd8e
JPGodinho <jpgpassiani@gmail.com>
2026-04-07 00:47:28 -0300

    fix: arrumando delivery

--- src/Aula02/Delivery.java
+++ src/Aula02/Delivery.java
@@ -1,33 +1,26 @@
 package Aula02;
 
 import Aula02.cliente.Cliente;
-
 import java.util.Scanner;
-import Aula02.cliente.Cliente;
 
 
 public class Delivery {
     static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args){
-        System.out.println("Seja bem vindo!");
-
-//        Cliente joao = new Cliente("João", "jpni@gmail.com", "11994577384");
-//        Endereco endereco1 = new Endereco("Rua Jose", "123", "Algumnis", "Saosao", "230498392");
-//        System.out.println("Entrega em: " + endereco1.mostrarCompleto());
-
-        Cliente cliente = Delivery.cadastrarCliente();
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao sistema");
+        Cliente cliente = cadastrarCliente();
     }
 
-    static cadastrarCliente(){
+    static  Cliente cadastrarCliente(){
         System.out.println("Iniciando cadastro de cliente");
         System.out.println("Digite o nome do cliente");
         String nome = scanner.nextLine();
-        System.out.println("Digite o email do cliente");
+
+        System.out.println("Digite o email");
         String email = scanner.nextLine();
-        System.out.println("Digite o telefone do cliente");
+
+        System.out.println("Digite o Telefone");
         String telefone = scanner.nextLine();
-
         return new Cliente(nome, email, telefone);
-
-    }
+    };
 }
